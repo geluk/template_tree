2c2095d8184c2bb31ddc02812730a88f2db8fa6c
Johan Geluk <johan@geluk.io>
2021-08-14 20:54:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# Introduction
+
+`template_tree` is an Ansible module that lets you
+replicate a directory tree of templates and files from your local host to a
+remote  host. It will automatically create directory entries where required, and
+can optionally remove files not present in the source directory.
+
+The main purpose of this module is to simplify the common scenario of
+copying one or more local files to a target directory, checking for files that
+were already present on the server but not on your host, and then removing those
+files. In a way, it's like `rsync`, but for Ansible.
+
+Let's say you want to create the following configuration structure on a
+webserver:
+```
+/etc/nginx
+├── http.conf.d
+│   ├── site.conf
+│   ├── wiki.conf
+│   └── fileserver.conf
+├── nginx.conf
+├── ssl.conf
+├── proxy_settings.conf
+├── mime.types
+└── ffdhe4096.pem
+
+/srv/http/static
+├── index.html
+├── blog
+│   ├── index.html
+│   ├── post1.html
+│   ├── post2.html
+│   └── post3.html
+└── about
+    └── index.html
+```
+`http.conf.d` contains one configuration file for each site you want to serve.
+These will be different on each host.
+
+`nginx.conf` is the main nginx configuration file, which is largely the same on
+each host, but needs to be templated with a few variables. The same applies to
+`ssl.conf`.
+
+`proxy_settings.conf` contains proxy header settings for reverse proxies. This
+file has the same content on all hosts.
+
+`/srv/http/static` contains static files served from one of your sites. The
+contents of this directory will vary, with new directories and files being
+added or removed over time.
+
+Now, if your `httpserver` role looks like this:
+```
+files
+├── hosts
+│   └── webserver01
+│       ├── sites
+│       │   ├── site.conf
+│       │   ├── wiki.conf
+│       │   └── fileserver.conf
+│       └── static_files
+│           ├── index.html
+│           ├── blog
+│           │   ├── index.html
+│           │   ├── post1.html
+│           │   ├── post2.html
+│           │   └── post3.html
+│           └── about
+│               └── index.html
+└── ffdhe4096.pem
+
+roles/httpserver
+├── tasks
+│   └── main.yml
+├── files
+    ├── mime.types
+    └── proxy_settings.conf.j2
+└── templates
+    ├── nginx.conf.j2
+    └── ssl.conf.j2
+```
+... then all you need is the following tasks to generate the aforementioned
+nginx configuration structure.
+```yaml
+# tasks/main.yml
+
+- name: Installing configuration files
+  template_tree:
+    # Look for files and templates in the following directories.
+    # Their contents will be merged together into the destination directory.
+    src:
+    # These entries respect the Ansible search order, so the module will start
+    # looking in the role directory before moving on to the play directory.
+    # These directories are fetched from the role:
+    - templates/
+    - files/
+    # This file is not present in the role, and is fetched from the play files.
+    - ffdhe4096.pem
+    # Copy or template discovered files to the following directory.
+    dest: /etc/nginx
+    # Any copied or templated files will receive the following mode.
+    file_mode: 0644
+    # Created directories will be set to this mode.
+    directory_mode: 0755
+
+- name: Installing websites
+  template_tree:
+    # No need to use a list if you only have a single source directory/file.
+    src: hosts/{{ inventory_hostname_short }}/sites/
+    dest: /etc/nginx/conf.d
+    # Setting 'exclusive' to true will ensure any files not present on the local
+    # host are removed from the destination directory.
+    exclusive: true
+    file_mode: 0644
+    directory_mode: 0755
+
+- name: Installing static files
+  template_tree:
+    # Postfix directories with a slash to copy only their contents. Leave
+    # the slash out to also generate the source directory on the host.
+    # (just like rsync)
+    src: hosts/{{ inventory_hostname_short }}/static_files/
+    dest: /srv/http/static
+    exclusive: true
+    file_mode: 0644
+    directory_mode: 0755
+```
+
+Note that any file ending with a `.j2` extension will be templated, and the
+extension will be removed.
+
+# Installation
+
+Drop `template_tree.py` into your action plugin directory. Normally this will be
+the `action_plugins` directory in the directory that contains your playbooks.
+
+For more information, see [adding a plugin locally](https://docs.ansible.com/ansible/latest/dev_guide/developing_locally.html#adding-a-plugin-locally)
+on Ansible.
---
+++ template_tree.py
@@ -0,0 +1,406 @@
+#!/usr/bin/python
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.action import ActionBase
+from pathlib import PurePath
+
+import os
+import os.path as path
+import stat
+
+import ansible.module_utils.common.text.converters as converters
+
+
+class ActionModule(ActionBase):
+    REQUIRED_ARGS = ("src", "dest")
+    TEMPLATE_EXTENSION = ".j2"
+
+    def run(self, tmp=None, task_vars=None):
+        output = super(ActionModule, self).run(tmp, task_vars)
+
+        for arg in self.REQUIRED_ARGS:
+            if not arg in self._task.args:
+                output["failed"] = True
+                output["msg"] = f"missing required argument '{arg}'"
+                return output
+
+        remote_path = self._task.args["dest"]
+
+        local_paths = self._task.args["src"]
+        if not isinstance(local_paths, list):
+            local_paths = [local_paths]
+
+        owner = self._task.args.get("owner", None)
+        group = self._task.args.get("group", None)
+        file_mode = self._task.args.get("file_mode", None)
+        directory_mode = self._task.args.get("directory_mode", None)
+        exclusive = self._task.args.get("exclusive", False)
+        exclusive_ignore = self._parse_path_list("exclusive_ignore")
+
+        self._display.vv(f"TEMPLATE_TREE: {local_paths} -> {remote_path}")
+
+        local_entries = self._get_local_entries(local_paths, task_vars)
+        remote_entries = self._get_remote_entries(remote_path, task_vars)
+
+        to_create = list(
+            self._get_entries_to_create(
+                local_entries, remote_path, owner, group, file_mode, directory_mode
+            )
+        )
+
+        self._set_file_contents(to_create, task_vars)
+
+        to_delete = list(
+            self._get_entries_to_delete(
+                to_create, remote_entries, remote_path, exclusive, exclusive_ignore
+            )
+        )
+
+        results = list(self._delete_entries(to_delete, task_vars))
+        results.extend(self._create_entries(to_create, task_vars))
+
+        self._build_output(output, results)
+        return output
+
+    def _parse_path_list(self, argname):
+        paths = self._task.args.get(argname, [])
+        is_list = True
+        if not isinstance(paths, list):
+            paths = [paths]
+            is_list = False
+
+        try:
+            return list(map(PurePath, paths))
+        except:
+            if is_list:
+                raise AnsibleError(
+                    f"Argument '{argname}' contains one or more values of an invalid type"
+                )
+            else:
+                raise AnsibleError(f"Argument '{argname}' is of an invalid type")
+
+    def _get_local_entries(self, local_paths, task_vars):
+        entries = []
+        for local_path in local_paths:
+            basedir = task_vars.get("role_path", self._loader.get_basedir())
+            found_path = self._loader.path_dwim_relative(basedir, "files", local_path)
+            self._display.vv(f"RESOLVE_SRC: '{local_path}' -> '{found_path}'")
+
+            # We'll receive a reference to either a file or a directory.
+            # If it's a file, return it directly instead of searching through it.
+            if path.exists(found_path):
+                stat_result = os.lstat(found_path)
+                if stat.S_ISREG(stat_result.st_mode):
+                    # Path points to a regular file
+                    entries.append(
+                        dict(
+                            root=path.dirname(found_path) + path.sep,
+                            path=path.basename(found_path),
+                            state="file",
+                            src=found_path,
+                        )
+                    )
+                    continue
+
+            # If the source path does not end with a directory separator, we must
+            # copy the source directory including its contents. filetree does not
+            # support also retrieving the source directory, so we insert our own
+            # entry here.
+            # if not local_path.endswith(path.sep):
+            entries.append(
+                dict(
+                    root=local_path,
+                    path="",
+                    state="directory",
+                ),
+            )
+
+            filetree_lookup = self._shared_loader_obj.lookup_loader.get(
+                "community.general.filetree", loader=self._loader
+            )
+            # Filetree supports passing multiple paths, and will handle duplicates
+            # for us, by only returning the first entry it finds for a given
+            # relative path.
+            entries += filetree_lookup.run(local_paths, variables=task_vars)
+
+        # Be explicit about the keys we use.
+        filetree_used_keys = {"root", "path", "state", "src"}
+        entries = [
+            {key: value for key, value in entry.items() if key in filetree_used_keys}
+            for entry in entries
+        ]
+
+        return entries
+
+    def _get_remote_entries(self, remote_path, task_vars):
+        result = self._execute_module(
+            module_name="ansible.builtin.find",
+            module_args=dict(
+                file_type="any",
+                hidden=True,
+                paths=remote_path,
+                recurse=True,
+            ),
+            task_vars=task_vars,
+            tmp=None,
+        )
+
+        if "warnings" in result:
+            for warning in result["warnings"]:
+                self._display.warning(warning)
+
+        # If there are warnings returned by the module, they may also be present
+        # in the message, so we only check the message if we haven't printed
+        # any warnings yet.
+        elif "msg" in result and result["msg"]:
+            self._display.warning(result["msg"], formatted=True)
+
+        # Be explicit about the keys we use.
+        filetree_used_keys = {"path", "isdir", "isreg"}
+        entries = [
+            {key: value for key, value in entry.items() if key in filetree_used_keys}
+            for entry in result["files"]
+        ]
+
+        for entry in entries:
+            entry["path"] = PurePath(entry["path"])
+
+        return entries
+
+    def _get_entries_to_create(
+        self, local_entries, remote_path, owner, group, file_mode, directory_mode
+    ):
+        for local_entry in local_entries:
+            relative_path = path.normpath(local_entry["path"])
+
+            destination_root = remote_path
+            if not local_entry["root"].endswith(path.sep):
+                source_dir = path.basename(local_entry["root"])
+                destination_root = path.join(destination_root, source_dir)
+
+            destination_path = path.join(destination_root, relative_path)
+            # When processing the root dir, relative_path will be '.', which
+            # will end up as '/.' at the end of destination_path.
+            # Normalising it one more time will get rid of this.
+            destination_path = path.normpath(destination_path)
+
+            entry = {
+                "dest": destination_path,
+                "state": local_entry["state"],
+                "owner": owner,
+                "group": group,
+            }
+            if entry["state"] == "file":
+                # Only files have a 'src' key.
+                entry["src"] = local_entry["src"]
+                entry["template"] = False
+                entry["mode"] = file_mode
+
+                if destination_path.endswith(self.TEMPLATE_EXTENSION):
+                    entry["dest"] = destination_path[: -len(self.TEMPLATE_EXTENSION)]
+                    entry["template"] = True
+
+                self._display.vv(f"MAP_FILE: '{entry['src']} -> {entry['dest']}'")
+
+            elif entry["state"] == "directory":
+                self._display.vv(f"REMOTE_DIR: '{entry['dest']}'")
+                entry["mode"] = directory_mode
+            else:
+                self._display.warning(
+                    f"Ignoring unrecognised local file type: {entry['state']}"
+                )
+                continue
+
+            yield entry
+
+    def _set_file_contents(self, entries, task_vars):
+        for file in filter(lambda e: e["state"] == "file", entries):
+            if file["template"]:
+                self._display.vv(f"TEMPLATE: {file['src']}")
+                content = self._template_local_file_contents(file["src"], task_vars)
+            else:
+                self._display.vv(f"READ: {file['src']}")
+                content = self._get_local_file_contents(file["src"])
+
+            file["content"] = content
+
+    def _template_local_file_contents(self, path, task_vars):
+        self._display.vvvv(f"Template local file '{path}'")
+        template_lookup = self._shared_loader_obj.lookup_loader.get(
+            "ansible.builtin.template",
+            loader=self._loader,
+            templar=self._templar,
+        )
+        return template_lookup.run([path], variables=task_vars)[0]
+
+    def _get_local_file_contents(self, path):
+        self._display.vvvv(f"File lookup using '{path}' as file")
+        try:
+            contents, _ = self._loader._get_file_contents(path)
+            return converters.to_text(contents, errors="surrogate_or_strict")
+        except AnsibleParserError:
+            raise AnsibleError(f"could not locate file in lookup: {path}")
+
+    def _get_entries_to_delete(
+        self,
+        local_entries,
+        remote_entries,
+        remote_path,
+        exclusive,
+        exclusive_ignore,
+    ):
+        # For convenience, we'll allow specifying absolute ignore paths.
+        # We rewrite them to relative paths here, so our comparison of a
+        # relative path to another relative path below works.
+        exclusive_ignore = list(
+            map(
+                lambda i: i.relative_to(remote_path) if i.is_absolute() else i,
+                exclusive_ignore,
+            )
+        )
+
+        for remote_entry in remote_entries:
+            absolute_path: PurePath = remote_entry["path"]
+            relative_path = absolute_path.relative_to(remote_path)
+
+            # We could short-circuit on files here, if their parent is already
+            # being deleted. This will reduce the number of calls
+            # to the file module with state=absent.
+            local_match = next(
+                (
+                    local_entry
+                    for local_entry in local_entries
+                    if PurePath(local_entry["dest"]) == absolute_path
+                ),
+                None,
+            )
+
+            if not local_match:
+                if exclusive:
+                    ignore_match = next(
+                        (
+                            ignore_path
+                            for ignore_path in exclusive_ignore
+                            if relative_path.is_relative_to(ignore_path)
+                        ),
+                        None,
+                    )
+                    if ignore_match:
+                        self._display.vv(
+                            f"COMPARE: keep '{relative_path}' (child of ignored path '{ignore_match}')"
+                        )
+                    else:
+                        self._display.vv(
+                            f"COMPARE: delete '{relative_path}' (absent in source)"
+                        )
+                        yield remote_entry
+                else:
+                    self._display.vv(
+                        f"COMPARE: keep '{relative_path}' (exclusive mode disabled)"
+                    )
+
+                continue
+
+            local_state = local_match["state"]
+            remote_state = None
+            if remote_entry["isdir"]:
+                remote_state = "directory"
+            elif remote_entry["isreg"]:
+                remote_state = "file"
+            else:
+                remote_state = "other"
+
+            if local_state != remote_state:
+                self._display.vv(
+                    f"COMPARE: delete '{relative_path}' (source is {remote_state}, destination is {local_state})"
+                )
+                yield remote_entry
+                continue
+
+            self._display.vv(f"COMPARE: keep '{relative_path}' (present in source)")
+
+    def _delete_entries(self, entries, task_vars):
+        for entry in entries:
+            self._display.vv(f"DELETE: {entry['path']}")
+            res = self._execute_module(
+                module_name="ansible.builtin.file",
+                module_args=dict(path=str(entry["path"]), state="absent"),
+                task_vars=task_vars,
+                tmp=None,
+            )
+
+            if res.get("failed", False):
+                raise AnsibleError(res["msg"])
+            yield res
+
+    def _create_entries(self, entries, task_vars):
+        for entry in entries:
+            if entry["state"] == "file":
+                result = self._copy_file(entry, task_vars)
+            elif entry["state"] == "directory":
+                result = self._create_directory(entry, task_vars)
+
+            if result.get("failed", False):
+                raise AnsibleError(result["msg"])
+            yield result
+
+    def _copy_file(self, file, task_vars):
+        task = self._task.copy()
+        task.args = dict(
+            content=file["content"],
+            dest=file["dest"],
+            group=file["group"],
+            owner=file["owner"],
+            mode=file["mode"],
+        )
+
+        self._display.vv(f"COPY: {file['dest']}")
+        copy_action = self._shared_loader_obj.action_loader.get(
+            "ansible.builtin.copy",
+            task=task,
+            connection=self._connection,
+            play_context=self._play_context,
+            loader=self._loader,
+            templar=self._templar,
+            shared_loader_obj=self._shared_loader_obj,
+        )
+        return copy_action.run(task_vars=task_vars)
+
+    def _create_directory(self, directory, task_vars):
+        self._display.vv(f"DIR: {directory['dest']}")
+        return self._execute_module(
+            module_name="ansible.builtin.file",
+            module_args=dict(
+                path=directory["dest"],
+                state="directory",
+                group=directory["group"],
+                owner=directory["owner"],
+                mode=directory["mode"],
+            ),
+            task_vars=task_vars,
+            tmp=None,
+        )
+
+    def _build_output(self, output, operation_results):
+        output["deleted_entries"] = []
+        output["managed_directories"] = []
+        output["managed_files"] = []
+        for result in operation_results:
+            if "state" in result:
+                if result["state"] == "absent":
+                    output["deleted_entries"].append(result)
+                elif result["state"] == "directory":
+                    output["managed_directories"].append(result)
+                elif result["state"] == "file":
+                    output["managed_files"].append(result)
+
+            output["changed"] = output.get("changed", False) or result["changed"]
+            if "diff" in result:
+                result_diff = result["diff"]
+                output_diff = output.setdefault("diff", [])
+
+                # Commands may return either a single diff, or a list of diffs.
+                if isinstance(result_diff, list):
+                    output_diff += result_diff
+                else:
+                    output_diff.append(result_diff)
